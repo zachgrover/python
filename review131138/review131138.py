54c7ba85fcea8cae9109cb99605f92e35fd7da71
zachgrover <z.grover@bssdstd.net>
2016-12-07 10:59:12 -0600

    Commit 12/7/2016

--- review131138/review131138.py
+++ review131138/review131138.py
@@ -1,3 +1,4 @@
+import random
 from Variables131138 import *
 def piglatin(names):
     vowels = ['A','E','I','O','U']
@@ -10,3 +11,17 @@ def piglatin(names):
             
     
     return pigLatinList
+    
+def nameGuesser(names):
+    print sorted(names)
+    theChosenOne = random.choice(names)
+    ri = 0
+    while ri != theChosenOne:
+        ri = raw_input('Who doth thou thinkest is le chosen one: ')
+        if ri > theChosenOne:
+            print 'the chosen one is before the name you guessed.'
+        elif ri < theChosenOne:
+            print ' the chosen one is after le name you guessed.'
+        else:
+            print 'you guessed le corectamoundo name.'
+            
